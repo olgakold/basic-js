6bd8dddbe0db0a302e79ddd5589f4c1571a67944
Olga Koldenkova <koldolga@gmail.com>
2021-03-15 19:12:38 +0300

    feat: add recursive-depth

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -9,7 +9,11 @@ if (!('separator' in options)){
     options.separator = '+'
 }
 
+if (!('additionSeparator' in options)){
+  options.additionSeparator = '|'
+}
+
 let str2 = (options.addition+options.additionSeparator).repeat(options['additionRepeatTimes']-1)+options.addition
 return (str+str2+options.separator).repeat(options.repeatTimes-1)+str+str2
 };
-  
+ 
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr ) {
+
+     let numbersArray
+     for (let i=0; i<arr.length; i++){
+       
+       if (Array.isArray(arr[i])){
+
+        numbersArray +=calculateDepth (arr[i])
+       }
+       else{
+         numbersArray = 1
+       }
+      
+     }
+
+     return numbersArray
   }
 };
